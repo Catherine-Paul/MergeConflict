48e13c5ba3116228f474fb45c76d46fbfbb1f358
admin <admin@192.168.2.227>
2014-01-30 19:46:10 -0600

    Checking in MergeProject.java

---
+++ src/mergeproject/MergeProject.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2014 Ballantine Communications, Inc., a Colorado corporation
+ * Describe requirements this class relates to.
+ * 0.0.0.000: Catherine Paul <at BDMedia> Jan 30, 2014 Initial file created
+ */
+
+package mergeproject;
+
+/**
+ *
+ * @author Catherine Paul <at BDMedia>
+ */
+public class MergeProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is the original line");
+    }
+    
+}
