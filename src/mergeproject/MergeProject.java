8d0e73988a819d2cdf80df9183103c87f65e19a0
Catherine <cthrnpaul@gmail.com>
2014-02-03 09:07:21 -0600

    Merge origin/master

---
+++ src/mergeproject/MergeProject.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014 Ballantine Communications, Inc., a Colorado corporation
+ * Describe requirements this class relates to.
+ * 0.0.0.000: Catherine Paul <at BDMedia> Jan 30, 2014 Initial file created
+ */
+
+package mergeproject;
+
+/**
+ *
+ * @author Catherine Paul <at BDMedia>
+ * 
+ * 
+ */
+
+
+
+public class MergeProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static class TestMerge
+{
+    String Y = new String("catherine");
+    String k = new String("Paul");
+    String Z = new String("Ambooken");
+    
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        TestMerge S = new TestMerge();
+        System.out.println(new TestMerge().Y);
+    }
+    
+}
